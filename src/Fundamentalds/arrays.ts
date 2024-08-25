49e66efcc7d7b3f9badddc77b3f1f580aa3efbc6
mouli-vj <moulevjg@gmail.com>
2024-08-25 23:53:56 +0530

    array declarations

---
+++ src/Fundamentalds/arrays.ts
@@ -0,0 +1,32 @@
+// Basics Declarations
+
+let prices: number[] = [100, 55, 75, 42];
+
+let fruits: string[] = ["Apple", "Orange"];
+
+let randomValues: [] = [];
+
+let empty: number[] = [];
+
+let names = ["Peter", "Susan"];
+let array: (string | boolean)[] = [true, "apple"];
+
+let temperature: number[] = [20, 24, 34];
+// temperature.push("hot");
+
+let colors: string[] = ["red", "green", "blue"];
+// colors.push(true)
+
+let mixedArray: (number | string)[] = [1, 2, "three"];
+
+// Object Fundamentals
+
+type Props = {
+  brand: string;
+  year: number;
+};
+
+let car: Props = {
+  brand: "Toyota",
+  year: 2020,
+};
