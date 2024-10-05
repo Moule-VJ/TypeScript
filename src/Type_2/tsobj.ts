4a7858f8ec8bbd715bd40b4ede6f05d93f89a243
mouli-vj <moulevjg@gmail.com>
2024-10-05 21:17:49 +0530

    objects fundamental 2

--- src/Type_2/tsobj.ts
+++ src/Type_2/tsobj.ts
@@ -13,10 +13,3 @@ let book1 = { title: "pen", cost: 20 };
 let note = { title: "note" };
 
 let items: { readonly title: string; cost?: number }[] = [book, book1, note];
-
-// challenge
-
-let bike: { brand: string; year: number } = { brand: "Yamaha", year: 2019 };
-// bike.year = "old";
-let laptop: { brand: string; year: number } = { brand: "acer", year: 2029 };
-// let laptop2: { brand: string; year: number } = { brand: "acer"  };
